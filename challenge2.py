fdd500219fce6fd75f4a46f397bd78de19ed3ccf
Corey Quinn <corey@sequestered.net>
2013-06-16 09:57:35 -0700

    Challenge 2 was a bit harder

---
+++ challenge2.py
@@ -0,0 +1,8 @@
+import string
+
+addtwo = string.maketrans(
+        "abcdefghijklmnopqrstuvwxyz",
+        "cdefghijklmnopqrstuvwzyzab")
+print string.translate( "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.", addtwo)
+
+print string.translate("map.html", addtwo)
